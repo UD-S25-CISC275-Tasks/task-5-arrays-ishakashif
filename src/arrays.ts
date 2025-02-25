7bf801bb7dab9de396f4ad17a0f3d2d406012730
Isha Kashif <ishak@udel.edu>
2025-02-24 19:38:06 -0500

    Solved makeMath

--- src/arrays.ts
+++ src/arrays.ts
@@ -73,7 +73,13 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    if (colors.length === 0) {
+        return true;
+    } else {
+        return colors.every(
+            (color) => color === "red" || color === "blue" || color === "green",
+        );
+    }
 }
 
 /**
@@ -84,7 +90,12 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    if (addends.length === 0) {
+        return "0=0";
+    }
+    const sum = addends.reduce((total: number, num: number) => total + num, 0);
+    const expression = addends.join("+");
+    return sum + "=" + expression;
 }
 
 /**
